98ef74c924a9d35abe8d030a04dfed227e22838f
Shaikh-Saqlain-Ahmad <98037915+Shaikh-Saqlain-Ahmad@users.noreply.github.com>
2023-05-11 12:09:43 +0500

    Update list.dart

--- list.dart
+++ list.dart
@@ -1,1 +1,19 @@
-
+/* Q 3.Initialize a list of Imtiaz Super Store Grocery Product Names only with 3 names
+and create a function which asks a user to add a new name to the list, until the user says NO, keep asking in the loop.*/
+import "dart:io";
+import "dart:core";
+var productName=["t-shirts","coffee","cold drinks"];
+String? item;
+void main(){
+    while(true){
+        print("enter the item you want or press n and enter");
+        String? item=stdin.readLineSync();
+        if(item=="n")
+        break;
+        else{
+            productName.add(item!);
+        }
+    }
+    print("the final item list is $productName");
+    
+}
