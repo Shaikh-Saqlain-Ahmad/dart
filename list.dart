217e61b666e7b5238db8fc1ca004f7bf7cd0f77c
Shaikh-Saqlain-Ahmad <98037915+Shaikh-Saqlain-Ahmad@users.noreply.github.com>
2023-05-14 12:12:23 +0500

    Update list.dart

--- list.dart
+++ list.dart
@@ -17,3 +17,34 @@ void main(){
     print("the final item list is $productName");
     
 }
+/*Q 4.Create another function for above task which asks the user to remove last element
+ of productNamesList until the user says NO, keep asking and removing.*/
+import "dart:io";
+import "dart:core";
+var productName=["t-shirts","coffee","cold drinks"];
+String? item;
+void main(){
+    while(true){
+        print("enter the item you want or press n and enter");
+        String? item=stdin.readLineSync();
+        if(item=="n")
+        break;
+        else{
+            productName.add(item!);
+        }
+    }
+    print("the final item list is $productName");
+    String? removal;
+    while(true){
+        print("enter the item you want to delete or press n and enter ");
+        String? removal=stdin.readLineSync();
+        if(removal=="n")
+        break;
+        else{
+            productName.remove(removal!);
+        }
+        
+    }
+    print("the final list is $productName");
+}
+
